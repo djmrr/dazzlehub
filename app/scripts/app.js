76f3d048e541c45b39332ebd35b1934368c1eda9
djmrr <mikedjrussell@gmail.com>
2015-07-05 18:31:17 -0600

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dazzlehubApp
+ * @description
+ * # dazzlehubApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dazzlehubApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
